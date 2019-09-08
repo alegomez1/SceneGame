7dbecd13513b890e6fe0a43df2446dad1742c5e7
alejandrogomez <alegomez1@me.com>
2019-09-08 15:39:27 -0400

    Working state without any audio

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -70,6 +70,118 @@ class gameScene extends Phaser.Scene {
 
     }
 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    preload() {
+        this.load.image('rocket', rocket)
+        this.load.image('rocketD1', rocketD1)
+        this.load.image('rocketD2', rocketD2)
+        this.load.image('rocketD3', rocketD3)
+        this.load.image('rocketD4', rocketD4)
+        this.load.image('asteroid', asteroid)
+        this.load.image('astronaut', astronautIMG)
+        this.load.image('fuelCan', fuelCan)
+        this.load.image('healthPack', healthPack)
+        // this.load.audio("ambient", ambient)
+        // this.load.audio("refuel", refuelSOUND)
+        // this.load.audio("femaleThanks", femaleThanksSOUND)
+        // this.load.audio("maleThanks", maleThanksSOUND)
+        // this.load.audio("maleThanks2", maleThanks2SOUND)
+        // this.load.audio("crash1", crash1SOUND)
+        // this.load.audio("healthSound", healthSoundSOUND)
+
+        // this.load.multiatlas('rocket2', atlast)
+    }
+
+    create() {
+        player = this.physics.add.sprite(35, 250, 'rocket')
+        console.log("WORK")
+        //Music
+        // music = this.sound.add("ambient")
+        // refuel = this.sound.add("refuel")
+        // rocketSound = this.sound.add("rocketSound")
+        // femaleThanks = this.sound.add('femaleThanks')
+        // maleThanks = this.sound.add('maleThanks')
+        // maleThanks2 = this.sound.add('maleThanks2')
+        // crash1 = this.sound.add("crash1")
+        // healthSound = this.sound.add('healthSound')
+        // var musicConfig = {
+        //     mute: false,
+        //     volume: 1,
+        //     rate: 1,
+        //     detune: 0,
+        //     seek: 0,
+        //     loop: true,
+        //     delay: 0
+        // }
+        // music.play(musicConfig)
+
+        //Player(Rocket)
+
+
+        // this.physics.add.sprite(35, 250, 'rocket2', 'RocketSpriteD4.png')
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+        player.setSize(24, 25)
+        player.setScale(1.3)
+        player.setDrag(1000);
+        player.setAngularDrag(900);
+        player.setMaxVelocity(600);
+        player.angle = -90
+        //Astronaut
+        astronaut = this.physics.add.group()
+        //Health Packs
+        healthPacks = this.physics.add.group()
+        //Asteroids
+        bigAsteroids = this.physics.add.group()
+        smallAsteroids = this.physics.add.group()
+        //Fuel Cans
+        fuelCans = this.physics.add.group()
+        //Collision Physics
+        this.physics.add.collider(player, rocketPad)
+        //Cursors
+        cursors = this.input.keyboard.createCursorKeys();
+        //Functions
+        this.createAsteroid()
+        this.createFuel()
+        this.createAstronauts()
+        this.createHealthPack()
+        //Adding Text
+        fuelText = this.add.text(16, 16, '', {
+            fontSize: '32px',
+            fill: '#FFFFFF'
+        });
+        savedText = this.add.text(800, 16, '', {
+            fontSize: '32px',
+            fill: '#FFFFFF'
+        })
+        healthText = this.add.text(16, 80, '', {
+            fontSize: '32px',
+            fill: '#FFFFFF'
+        })
+        gameOverText = this.add.text(400, 300, '', {
+            fontSize: '90px',
+            fill: '#FFFFFF'
+        })
+    }
+
+
+
+
     createAstronauts() {
         setInterval(function () {
             var strandedAstronaut = astronaut.create(1450, Phaser.Math.Between(0, 700), 'astronaut')
@@ -167,118 +279,13 @@ class gameScene extends Phaser.Scene {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    preload() {
-        this.load.image('rocket', rocket)
-        this.load.image('rocketD1', rocketD1)
-        this.load.image('rocketD2', rocketD2)
-        this.load.image('rocketD3', rocketD3)
-        this.load.image('rocketD4', rocketD4)
-        this.load.image('asteroid', asteroid)
-        this.load.image('astronaut', astronautIMG)
-        this.load.image('fuelCan', fuelCan)
-        this.load.image('healthPack', healthPack)
-        // this.load.audio("ambient", ambient)
-        // this.load.audio("refuel", refuelSOUND)
-        // this.load.audio("femaleThanks", femaleThanksSOUND)
-        // this.load.audio("maleThanks", maleThanksSOUND)
-        // this.load.audio("maleThanks2", maleThanks2SOUND)
-        // this.load.audio("crash1", crash1SOUND)
-        // this.load.audio("healthSound", healthSoundSOUND)
-
-        // this.load.multiatlas('rocket2', atlast)
-    }
-
-    create() {
-        player = this.physics.add.sprite(35, 250, 'rocket')
-        console.log("WORK")
-        //Music
-        // music = this.sound.add("ambient")
-        // refuel = this.sound.add("refuel")
-        // rocketSound = this.sound.add("rocketSound")
-        // femaleThanks = this.sound.add('femaleThanks')
-        // maleThanks = this.sound.add('maleThanks')
-        // maleThanks2 = this.sound.add('maleThanks2')
-        // crash1 = this.sound.add("crash1")
-        // healthSound = this.sound.add('healthSound')
-        // var musicConfig = {
-        //     mute: false,
-        //     volume: 1,
-        //     rate: 1,
-        //     detune: 0,
-        //     seek: 0,
-        //     loop: true,
-        //     delay: 0
-        // }
-        // music.play(musicConfig)
-
-        //Player(Rocket)
-
-
-        // this.physics.add.sprite(35, 250, 'rocket2', 'RocketSpriteD4.png')
-        player.setBounce(0.2);
-        player.setCollideWorldBounds(true);
-        player.setSize(24, 25)
-        player.setScale(1.3)
-        player.setDrag(1000);
-        player.setAngularDrag(900);
-        player.setMaxVelocity(600);
-        player.angle = -90
-        //Astronaut
-        astronaut = this.physics.add.group()
-        //Health Packs
-        healthPacks = this.physics.add.group()
-        //Asteroids
-        bigAsteroids = this.physics.add.group()
-        smallAsteroids = this.physics.add.group()
-        //Fuel Cans
-        fuelCans = this.physics.add.group()
-        //Collision Physics
-        this.physics.add.collider(player, rocketPad)
-        //Cursors
-        cursors = this.input.keyboard.createCursorKeys();
-        //Functions
-        createAsteroid()
-        createFuel()
-        createAstronauts()
-        createHealthPack()
-        //Adding Text
-        fuelText = this.add.text(16, 16, '', {
-            fontSize: '32px',
-            fill: '#FFFFFF'
-        });
-        savedText = this.add.text(800, 16, '', {
-            fontSize: '32px',
-            fill: '#FFFFFF'
-        })
-        healthText = this.add.text(16, 80, '', {
-            fontSize: '32px',
-            fill: '#FFFFFF'
-        })
-        gameOverText = this.add.text(400, 300, '', {
-            fontSize: '90px',
-            fill: '#FFFFFF'
-        })
-    }
-
     update() {
         //Adds overlap physics to player and fuelcans
-        this.physics.add.overlap(player, fuelCans, collectFuel, null, this);
-        this.physics.add.overlap(player, astronaut, rescue, null, this);
-        this.physics.add.overlap(player, bigAsteroids, crashBig, null, this);
-        this.physics.add.overlap(player, smallAsteroids, crashSmall, null, this);
-        this.physics.add.overlap(player, healthPacks, collectHealthPack, null, this);
+        this.physics.add.overlap(player, fuelCans, this.collectFuel, null, this);
+        this.physics.add.overlap(player, astronaut, this.rescue, null, this);
+        this.physics.add.overlap(player, bigAsteroids, this.crashBig, null, this);
+        this.physics.add.overlap(player, smallAsteroids, this.crashSmall, null, this);
+        this.physics.add.overlap(player, healthPacks, this.collectHealthPack, null, this);
 
         var rocketConfig = {
             mute: false,
@@ -292,14 +299,12 @@ class gameScene extends Phaser.Scene {
 
         //Movement
         if (cursors.left.isDown && fuel > 0) {
-            rocketSound.play(rocketConfig)
-            rocketSound.stop(rocketConfig)
+           
             // player.setVelocityX(-160);
             player.setAngularVelocity(-200);
 
         } else if (cursors.right.isDown && fuel > 0) {
-            rocketSound.play(rocketConfig)
-            rocketSound.stop(rocketConfig)
+           
             // player.setVelocityX(160)
             player.setAngularVelocity(200);
 
@@ -308,8 +313,7 @@ class gameScene extends Phaser.Scene {
         }
 
         if (cursors.up.isDown && fuel > 0) {
-            rocketSound.play(rocketConfig)
-            rocketSound.stop(rocketConfig)
+           
             // player.setVelocityY(-200)
             this.physics.velocityFromRotation(player.rotation, 300, player.body.acceleration);
             fuel--
